4a123bb1d5075fedf9004212918ae57ae39274a5
euginiagabrielle <icin2004@gmail.com>
2026-06-01 16:17:41 +0700

    feat: unit akademik crud with filter

--- service.py
+++ service.py
@@ -6,6 +6,7 @@ from models import UnitAkademik
 class MasterService:
     name = "master_service"
 
+    # UNIT AKADEMIK
     @rpc
     def create_unit(self, name, type, parent_id=None):
         db = SessionLocal()
@@ -28,7 +29,7 @@ class MasterService:
             db.close()
     
     @rpc
-    def get_units(self):
+    def get_all_units(self):
         db = SessionLocal()
         units = db.query(UnitAkademik).all()
 
@@ -42,5 +43,57 @@ class MasterService:
                 }
                 for u in units
             ]
+        finally:
+            db.close()
+    
+    @rpc
+    def get_unit_by_id(self, unit_id):
+        db = SessionLocal()
+        try:
+            unit = db.query(UnitAkademik).filter(UnitAkademik.unit_id == unit.id).first()
+            if not unit:
+                return {"status": "error", "message": "Unit Akademik tidak ditemukan"}
+            return {
+                "status": "success",
+                "data": {"id": unit.unit_id, "name": unit.unit_name, "type": unit.unit_type, "parent_id": unit.parent_id}
+            }
+        finally:
+            db.close()
+    
+    @rpc
+    def update_unit(self, unit_id, name=None, type=None, parent_id=None):
+        db = SessionLocal()
+        try:
+            unit = db.query(UnitAkademik).filter(UnitAkademik.unit_id == unit_id).first()
+            if not unit:
+                return {"status": "error", "message": "Unit Akademik tidak ditemukan"}
+            
+            if name: unit.unit_name = name
+            if type: unit.unit_type = type
+            if parent_id is not None: unit.parent_id = parent_id
+
+            db.commit()
+            db.refresh(unit)
+            return {"status": "success", "message": "Data berhasil diupdate"}
+        except Exception as e:
+            db.rollback()
+            return {"status": "error", "message": str(e)}
+        finally:
+            db.close()
+
+    @rpc
+    def delete_unit(self, unit_id):
+        db = SessionLocal()
+        try:
+            unit = db.query(UnitAkademik).filter(UnitAkademik.unit_id == unit_id).first()
+            if not unit:
+                return {"status": "error", "message": "Unit Akademik tidak ditemukan"}
+            
+            db.delete(unit)
+            db.commit()
+            return {"status": "success", "message": f"Unit {unit.unit_name} berhasil dihapus"}
+        except Exception as e:
+            db.rollback()
+            return {"status": "error", "message": "Gagal menghapus, mungkin ada data turunan yang mengikat. " + str(e)}
         finally:
             db.close()
